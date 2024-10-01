ddae07a7578957301afcd2ce258fefbfceca96e0
Khiran Gannu <117846037+Mangocod3r@users.noreply.github.com>
2024-10-01 07:35:20 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,15 +1,19 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 const IP = "129.154.227.35";
-const PORT = "43210";
-// change above IP PORT ONLY 
-async function handler(req: Request): Promise<Response> {
- 
+const PORT = "43210"; // change above IP PORT ONLY 
+
+async function handler(req) {
   const url = req.url;
   const body = await req.text();
   const path = url.substring(url.indexOf('.dev') + 4);
   const method = req.method;
-  return await fetch(`http://${IP}:${PORT}${path}`, {method: req.method, body: method !== 'GET' && method !== 'HEAD' ? body : undefined,
-            headers: new Headers(req.headers)});
+
+  return await fetch(`http://${IP}:${PORT}${path}`, {
+    method: req.method,
+    body: method !== 'GET' && method !== 'HEAD' ? body : undefined,
+    headers: new Headers(req.headers),
+  });
 }
+
 serve(handler);
